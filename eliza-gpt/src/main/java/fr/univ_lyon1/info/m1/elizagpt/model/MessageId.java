a5749e68af9e479c52612edf4716058a22ea2217
Stéphane <stephane.bomard@etu.univ-lyon1.fr>
2023-11-02 11:30:26 +0100

    Simple refonte de l'indentation.
    C'est une mauvais pratique mais elle est ici faites de manière
    exceptionnelle.

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/MessageId.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/model/MessageId.java
@@ -1,18 +1,18 @@
 package fr.univ_lyon1.info.m1.elizagpt.model;
 
 public class MessageId {
-        private final String m_message;
-        private final int m_id;
+	private final String m_message;
+	private final int m_id;
 
-        public MessageId(final String message, final int id) {
-                m_message = message;
-                m_id = id;
-        } 
+	public MessageId(final String message, final int id) {
+		m_message = message;
+		m_id = id;
+	} 
 
-        public final String getMessage() {
-                return m_message;
-        }
-        public final int getId() {
-                return m_id;
-        }
+	public final String getMessage() {
+		return m_message;
+	}
+	public final int getId() {
+		return m_id;
+	}
 }
