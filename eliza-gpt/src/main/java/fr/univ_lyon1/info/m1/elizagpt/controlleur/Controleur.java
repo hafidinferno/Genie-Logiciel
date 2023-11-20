50b94c14a275df8632bf9ce6d3570adf206ed3c1
OUCOUC Hafid <hafid.oucouc@etu-lyon1.fr>
2023-11-20 01:28:13 +0100

    adding thee changes to handleMessage

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/controlleur/Controleur.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/controlleur/Controleur.java
@@ -2,32 +2,50 @@ package fr.univ_lyon1.info.m1.elizagpt.controlleur;
 
 
 import fr.univ_lyon1.info.m1.elizagpt.view.JfxView;
+import fr.univ_lyon1.info.m1.elizagpt.model.Dao;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javafx.scene.control.Label;
 
 
-/** La classe controleur qui s'occupe de relier entre la vue et le model. */
+/** La classe controleur qui s'occupe de gérer les interactions entre la vue et le model. */
 public final class Controleur {
-        private static Controleur instance;
-        private final int userId;
         private final HandleMessage handleMessage;
         private JfxView view;
+        private final Dao dao;
 
-        // Private constructor
-        private Controleur(final HandleMessage handleMessage) {
+
+
+        /**constructeur de la classe Controleur. */
+        public Controleur(final HandleMessage handleMessage,JfxView view) {
             this.handleMessage = handleMessage;
-            userId = 1;
+            this.view=view;
+            dao = Dao.getInstance();
+
         }
 
-    /** la methode handleUserREply s'occupe de stocker le message dans la Dao.
+    /** la methode handleUserReply s'occupe de stocker le message dans la Dao.
      * puis le traiter avec la fonction iaRespond de la classe handleMessage
      * puis l'afficher dans la vue.
      * @param message
      */
-    public void handleUserReply(final String message) {
-            // Store the message in the database
+    public void  handleUserReply(final String message) {
+            // ajouter le message à la base de données
             handleMessage.reply(message, userId);
 
-            // Process the message using HandleMessage
+            // Traitement de message
             handleMessage.iaRespond(message);
+
+            // notifier l'utilisateur qu'il y'a une recherche en cours
+            handleMessage.searchText( String message, Label searchTextLabel)
+
+
+    }
+
+
+        public void updateView(String message){
+                view.sendMessage(handleMessage.iaRespond(message));
 
         }
 
--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/controlleur/HandleMessage.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/controlleur/HandleMessage.java
@@ -29,8 +29,10 @@ public class HandleMessage {
      * @param id L'ID est celui de l'expéditeur.
      */
 	public void reply(final String text, final int id) {
+
         dao.addMessage(text, id);
 	// TODO: a click on this hbox should delete the message.
+
 	}
 
     /**
