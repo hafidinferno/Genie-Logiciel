be61bea6f4551560e74ae72a4d876294c45f6d7b
Stéphane <stephane.bomard@etu.univ-lyon1.fr>
2023-12-15 15:41:52 +0100

    Correction de typo.

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/controleur/HandleMessage.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/controleur/HandleMessage.java
@@ -6,7 +6,7 @@ import fr.univ_lyon1.info.m1.elizagpt.model.Dao;
 import java.util.ArrayList;
 
 /**
- * Controlleur des messages.
+ * Controleur des messages.
  */
 public class HandleMessage {
 	private final Dao dao;
@@ -22,7 +22,7 @@ public class HandleMessage {
 	}
 
 	/**
-     * Fonction de réponse qui sera utilisée par l'utilissateur
+     * Fonction de réponse qui sera utilisée par l'utilisateur
      * et l'IA.
      * @param text message.
      * @param isIa permet de savoir si le message a été envoyé par l'utilisateur ou l'IA.
@@ -42,10 +42,10 @@ public class HandleMessage {
 
     /**
      * Fonction de recherche via regex dans la database.
-	 * @param text chaine de caractère que l'on cherche
+     * @param text chaine de caractère que l'on cherche
      * @return Une liste de messages qui match avec notre recherche.
      */
-    public ArrayList<DataMessage> searchText(final String text) {
+    public ArrayList<DataMessage> search(final String text) {
         return dao.search(text);
     }
 }
